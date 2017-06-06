f7207ada45200135f1cfe05246afb6612c159d7d
Chengtao Ji <jctwilliam@gmail.com>
2017-06-06 13:12:38 -0700

    Added a helper method to split maps

--- src/cse291/lsmdb/io/sstable/compaction/LevelManager.java
+++ src/cse291/lsmdb/io/sstable/compaction/LevelManager.java
@@ -2,7 +2,9 @@ package cse291.lsmdb.io.sstable.compaction;
 
 import cse291.lsmdb.io.sstable.blocks.*;
 import cse291.lsmdb.utils.Modification;
+import cse291.lsmdb.utils.Pair;
 
+import java.io.IOException;
 import java.util.*;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
@@ -86,8 +88,39 @@ public class LevelManager {
         //TODO rename and gc
     }
 
-    public ArrayList<Map<String, Modification>> compact(Map<String, Modification> toCompact) {
-        return null;
+    /**
+     * Method to split a Map of <String, Modification> to maps which will be merged into individual data blocks
+     * @param toCompact a Map of <String, Modification> to be split
+     * @return ArrayList of maps to be merged into every data block in this level
+     * @throws IOException in case of IO Exception
+     */
+    private ArrayList<Map<String, Modification>> split_map(Map<String, Modification> toCompact) throws IOException {
+
+        // Create the list of Maps and fill with empty Maps
+        ArrayList<Map<String, Modification>> split_maps = new ArrayList<>();
+        IndexBlockLoader indexBlockLoader = this.getIndexBlockLoader();
+        for(int i = 0; i < indexBlockLoader.getRanges().size(); i++){
+            split_maps.add(new TreeMap<>());
+        }
+
+        ArrayList<Pair<String, String>> ranges = indexBlockLoader.getRanges(); // Get dataBlock ranges
+
+        // We put all entries has rowKey less than the maximum value of current range to corresponding dataBlock
+        // Unless it is already the last block
+        int currentRangeIndex = 0;
+        String end = ranges.get(currentRangeIndex).right;
+
+        for (Map.Entry<String, Modification> pair : toCompact.entrySet()) {
+            String rowName = pair.getKey();
+            Modification colValue = pair.getValue();
+            if(rowName.compareTo(end) > 0 && currentRangeIndex < ranges.size()-1){
+                currentRangeIndex += 1;
+                end = ranges.get(currentRangeIndex).right;
+            }
+            split_maps.get(currentRangeIndex).put(rowName,colValue);
+        }
+
+        return split_maps;
     }
 
     public Descriptor getDesc() {
