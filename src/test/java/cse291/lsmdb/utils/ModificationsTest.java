a37f1e2b8c8a142d036fc86837ad673cd3a122b3
musteryu <913811121@qq.com>
2017-06-09 11:19:30 -0700

    change structure

--- src/test/java/cse291/lsmdb/utils/ModificationsTest.java
+++ src/test/java/cse291/lsmdb/utils/ModificationsTest.java
@@ -1,14 +1,39 @@
 package cse291.lsmdb.utils;
 
+import cse291.lsmdb.io.sstable.SSTableConfig;
+import org.junit.Assert;
+import org.junit.Assume;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
+
+import java.util.NoSuchElementException;
 
 /**
  * Created by musteryu on 2017/6/9.
  */
 public class ModificationsTest {
+    static Modifications mod;
+    static SSTableConfig config;
+
+    @BeforeClass
+    public static void init() {
+        config = SSTableConfig.defaultConfig();
+    }
+
+
+    @Before
+    public void initMod() {
+        mod = new Modifications(config.getBlockBytesLimit());
+    }
+
     @Test
     public void put() throws Exception {
-
+        try {
+            mod.put("a", Modification.put(Timed.now("b")));
+        } catch (NoSuchElementException e) {
+            Assume.assumeNoException(e);
+        }
     }
 
     @Test
